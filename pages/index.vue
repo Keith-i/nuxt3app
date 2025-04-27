2a231cc80191675aeb269082dc365039efc65303
keith <754781293@qq.com>
2025-04-27 12:52:33 +0800

    提交

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
     <h1>欢迎来到首页</h1>
-<<<<<<< HEAD
-    <AppAlert> 这是一个自动导入的组件44 </AppAlert>
-=======
-    <AppAlert> 这是一个自动导入的组件22 </AppAlert>
->>>>>>> parent of c281474 (提交)
+    <AppAlert> 这是一个自动导入的组件00 </AppAlert>
   </div>
 </template>
 
